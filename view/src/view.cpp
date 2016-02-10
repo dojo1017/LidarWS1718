62d7efe7e1577e854612fbf8c8aac3c2c3ea93ac
Constantin Ei <princefisher@gmx.de>
2016-02-10 07:04:50 +0000

    set camera to center

--- view/src/view.cpp
+++ view/src/view.cpp
@@ -21,9 +21,9 @@ View::View(std::vector<glm::vec3> &_points, std::vector<glm::vec3> &_faces) {
 
     Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
     ViewLookAt = glm::lookAt(
-                     // glm::vec3(0, 0, 0), // Camera is at (4,1,3), in World Space
-                     glm::vec3(4, 1, 3), // Camera is at (4,1,3), in World Space
-                     glm::vec3(0, 0, 0), // and looks at the origin
+                     glm::vec3(0, 0.5f, 0), // Camera is at origin in World Space
+                     // glm::vec3(4, 1, 3), // Camera is at (4,1,3), in World Space
+                     glm::vec3(1, 0.5f, 0), // and looks at direction x
                      glm::vec3(0, 1, 0) // Head is up (set to 0,-1,0 to look upside-down)
                  );
     // Model matrix : an identity matrix (model will be at the origin)
@@ -73,7 +73,7 @@ void View::startScreen() {
         glUniformMatrix4fv(ModelMatrixID, 1, GL_FALSE, &Model[0][0]);
         glUniformMatrix4fv(ViewMatrixID, 1, GL_FALSE, &ViewLookAt[0][0]);
 
-        glm::vec3 lightPos = glm::vec3(4, 4, 4);
+        glm::vec3 lightPos = glm::vec3(0, 0.5f, 0);
         glUniform3f(LightID, lightPos.x, lightPos.y, lightPos.z);
 
         // 1rst attribute buffer : points
