384dc2965f4c36e0efa96c192b338ff1394a82eb
Constantin Ei <princefisher@gmx.de>
2016-02-23 21:32:03 +0100

    letztes update. zwei varianten zur berechnung der punkte im raum.

--- calculation/src/calculation.cpp
+++ calculation/src/calculation.cpp
@@ -16,12 +16,11 @@ void Calculation::addPoint(position pos, unsigned int distance) {
 // struct position { int x,y,z; }; // Kompasswerte
 // unsigned int distance // Lidar Distanz
 
-// result = rot1(y) * trans1(y) * rot2(z) * trans2(y) * rot3(z) * trans3(y) * trans4(y->distance)
 // Servos von unten (1), mitte (2), oben (3)
 
-	float servo1 = 90.0f;
+	float servo1 = 0.0f;
 	float servo2 = 90.0f;
-	float servo3 = 90.0f;
+	float servo3 = 0.0f;
 
 
 	glm::vec3 pos3(pos.x, pos.y, pos.z);
@@ -29,17 +28,48 @@ void Calculation::addPoint(position pos, unsigned int distance) {
 	glm::vec4 hVector = glm::vec4(pos3, 1.0f); // create a homogen vector from the compass position
 
 
+	// result = rot1(y) * trans1(y) * rot2(z) * trans2(y) * rot3(z) * trans3(y) * trans4(y->distance)
+	// glm::mat4 idMat = glm::mat4(1.0f);
+	// glm::mat4 rotMat1 = glm::rotate(idMat, servo1, glm::vec3(0.0f, -1.0f, 0.0f)); // Rotation y axis
+	// glm::mat4 transMat1	= glm::translate(rotMat1, glm::vec3(0.0f, 8.5f, 0.0f)); // Translation y axis
+	// glm::mat4 rotMat2 = glm::rotate(transMat1, servo2, glm::vec3(0.0f, 0.0f, 1.0f)); // Rotation z axis
+	// glm::mat4 transMat2 = glm::translate(rotMat2, glm::vec3(0.0f, 7.5f, 0.0f)); // Translation y axis
+	// glm::mat4 rotMat3 = glm::rotate(transMat2, servo3, glm::vec3(0.0f, 0.0f, 1.0f));
+	// glm::mat4 transMat3 = glm::translate(rotMat3, glm::vec3(0.0f, 3.5f, 0.0f));
+	// glm::mat4 transMat4 = glm::translate(transMat3, glm::vec3(0.0f, distance, 0.0f));
+
+	// glm::vec4 hResult = transMat4 * hVector;
+
+	
 	glm::mat4 idMat = glm::mat4(1.0f);
-	glm::mat4 rotMat1 = glm::rotate(idMat, servo1, glm::vec3(0.0f, 1.0f, 0.0f)); // Rotation y axis
-	glm::mat4 transMat1	= glm::translate(rotMat1, glm::vec3(0.0f, 8.5f, 0.0f)); // Translation y axis
-	glm::mat4 rotMat2 = glm::rotate(transMat1, servo2, glm::vec3(0.0f, 0.0f, 1.0f)); // Rotation z axis
-	glm::mat4 transMat2 = glm::translate(rotMat2, glm::vec3(0.0f, 7.5f, 0.0f)); // Translation y axis
-	glm::mat4 rotMat3 = glm::rotate(transMat2, servo3, glm::vec3(0.0f, 0.0f, 1.0f));
-	glm::mat4 transMat3 = glm::translate(rotMat3, glm::vec3(0.0f, 3.5f, 0.0f));
+	
+	glm::mat4 rotMat1 = glm::rotate(idMat, servo1, glm::vec3(0.0f, -1.0f, 0.0f)); // Rotation y axis
+	glm::mat4 transMat1	= glm::translate(idMat, glm::vec3(0.0f, 8.5f, 0.0f)); // Translation y axis
 
-	glm::vec4 hResult = transMat3 * glm::vec4(0.0f, (float)distance, 0.0f, 1.0f);
+	glm::mat4 rotMat2 = glm::rotate(idMat, servo2, glm::vec3(0.0f, 0.0f, 1.0f)); // Rotation z axis
+	glm::mat4 transMat2 = glm::translate(idMat, glm::vec3(0.0f, 7.5f, 0.0f)); // Translation y axis
+	
+	glm::mat4 rotMat3 = glm::rotate(idMat, servo3, glm::vec3(0.0f, 0.0f, 1.0f));
+	glm::mat4 transMat3 = glm::translate(idMat, glm::vec3(0.0f, 3.5f, 0.0f));
+	
+	glm::mat4 transMat4 = glm::translate(idMat, glm::vec3(0.0f, distance, 0.0f));
+	
 
-	printf("\ntransMat3 = %s\n", glm::to_string(transMat3).c_str());
+	//         result = trans4(y) * trans3(y) * rot3(z) * trans2(y) * rot2(z) * trans1(y) * rot1(y) * position
+	glm::vec4 hResult = transMat4 * transMat3 * rotMat3 * transMat2 * rotMat2 * transMat1 * rotMat1 * hVector;
+
+
+	// printf("\nrotMat1 = %s\n", glm::to_string(rotMat1).c_str());
+
+	// printf("\ntransMat1 = %s\n", glm::to_string(transMat1).c_str());
+
+	// printf("\nrotMat2 = %s\n", glm::to_string(rotMat2).c_str());
+
+	// printf("\ntransMat2 = %s\n", glm::to_string(transMat2).c_str());
+
+	// printf("\nrotMat3 = %s\n", glm::to_string(rotMat3).c_str());
+
+	// printf("\ntransMat3 = %s\n", glm::to_string(transMat3).c_str());
 
 
 	printf("\nhResult = %s\n", glm::to_string(hResult).c_str());
