e8d612bf6db81b328e65d9fa770ea8196aa676cf
Simon Wendsche <simon.wendsche@gmx.de>
2018-03-06 17:33:21 +0100

    add code for starting, stopping and moving a motor

--- project_WS1718/Merlin.cpp
+++ project_WS1718/Merlin.cpp
@@ -4,6 +4,8 @@
 
 #include <stdio.h>
 #include <math.h>
+#include <sstream
+#include <iomanip>
 #include "Merlin.h"
 using std::string;
 
@@ -18,10 +20,7 @@ void Merlin::aimAt(float heading, float pitch) {
     double currPitch = gyro.getPitch();
     printf("Merlin: current heading %.2f pitch %.2f\n", currHeading, currPitch);
 
-
-    // TODO
-
-
+    
 
 //    while(true) {
 //        // Check where we are currently
@@ -50,14 +49,67 @@ void Merlin::aimAt(float heading, float pitch) {
 //    }
 }
 
-void Merlin::addCommand(string command) {
+void Merlin::addCommand(string command, bool lineEnd) {
     commands += command;
+    if(lineEnd) {
+        commands += "\r";
+    }
 }
 
 void Merlin::startMotor(string motor) {
-    addCommand(":G" + motor + "");
+    addCommand(":J" + motor);
 }
 
 void Merlin::stopMotor(string motor) {
-
+    addCommand(":L" + motor);
 }
+
+void Merlin::moveHeadingTo(float degrees) {
+    // The magic number 8388608 is from the Chronomotion code
+    long pos = degrees * stepsHeading / 360.f + 8388608;
+    string posString = positionToString(pos);
+
+    stopMotor(motorHeading);
+    addCommand(":G" + motorHeading + "40");
+    addCommand(":S" + motorHeading + posString);
+    startMotor(motorHeading);
+}
+
+string Merlin::positionToString(int pos) {
+    std::stringstream stream;
+    stream << std::setfill('0') << std::setw(6)
+           << std::uppercase
+           << std::hex << pos;
+    string temp(stream.str());
+
+    string result = temp;
+    result[0] = temp[4];
+    result[1] = temp[5];
+    result[2] = temp[2];
+    result[3] = temp[3];
+    result[4] = temp[0];
+    result[5] = temp[1];
+
+    return result;
+}
+
+//private String TranslatePosition(int pos) {
+//    String temp;
+//    temp = Integer.toHexString(pos);
+//    StringBuilder dest = new StringBuilder(temp);
+//    for (int i = temp.length(); i < 6; i++) {
+//        dest.append("0");
+//    }
+//
+//    StringBuilder dest2 = new StringBuilder("");
+//    dest2.append(dest.charAt(4));
+//    dest2.append(dest.charAt(5));
+//
+//    dest2.append(dest.charAt(2));
+//    dest2.append(dest.charAt(3));
+//
+//    dest2.append(dest.charAt(0));
+//    dest2.append(dest.charAt(1));
+//
+//    return dest2.toString().toUpperCase();
+//}
--- project_WS1718/Merlin.h
+++ project_WS1718/Merlin.h
@@ -15,14 +15,18 @@ public:
 
 private:
     Gyro gyro;
-    const float maxError = 1.f;
+//    const float maxError = 1.f;
     const std::string motorHeading = "1";
     const std::string motorPitch = "2";
+    const float stepsHeading = 1.f; // TODO
+    const float stepsPitch = 1.f; // TODO
     string commands;
 
-    void addCommand(std::string command);
+    void addCommand(std::string command, bool lineEnd=true);
     void startMotor(std::string motor);
     void stopMotor(std::string motor);
+    void moveHeadingTo(float degrees);
+    std::string positionToString(int pos);
 };
 
 
