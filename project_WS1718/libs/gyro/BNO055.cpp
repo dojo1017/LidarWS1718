4bd8ecc7bda151d160dbfb0b9a14f6bf62a44463
Yuta <angelo.douvere@gmail.com>
2018-03-07 16:13:18 +0100

    system allow calibration value 2

--- project_WS1718/libs/gyro/BNO055.cpp
+++ project_WS1718/libs/gyro/BNO055.cpp
@@ -587,7 +587,7 @@ bool BNO055::isFullyCalibrated(void)
 {
 	uint8_t system, gyro, accel, mag;
 	getCalibration(&system, &gyro, &accel, &mag);
-	if (system < 3 || gyro < 3 || accel < 3 || mag < 3)
+	if (system < 2 || gyro < 3 || accel < 3 || mag < 3)
 		return false;
 	return true;
 }
