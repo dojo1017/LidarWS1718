4e1f9c6460547230f147d53be5ba680e655f8a42
Simon Wendsche <simon.wendsche@gmx.de>
2018-03-09 11:39:36 +0100

    better debug prints

--- project_WS1718/main.cpp
+++ project_WS1718/main.cpp
@@ -8,6 +8,8 @@ using std::vector;
 
 // Take measurements while waiting for the motor to stop
 void measureLoop(Merlin &merlin, Lidar &lidar, vector<Measurement> &measurements, int MEASURE_INTERVAL_MS) {
+    int measureCount = 0;
+
     while(!merlin.hasMotorStopped(merlin.MOTOR_HEADING)) {
         // Take measurement with Lidar
         const unsigned int distanceCentimeters = lidar.measureDistance();
@@ -16,7 +18,10 @@ void measureLoop(Merlin &merlin, Lidar &lidar, vector<Measurement> &measurements
         measurements.emplace_back(Measurement(heading, pitch, distanceCentimeters));
         // Sleep some time between measurements (time in nanoseconds)
         usleep(MEASURE_INTERVAL_MS * 1000);
+        measureCount++;
     }
+
+    cout << "[LIDAR] " << measureCount << " measurements done" << endl;
 }
 
 int main(int argc, char **argv) {
@@ -58,23 +63,37 @@ int main(int argc, char **argv) {
 //    merlin.waitForStop(merlin.MOTOR_PITCH);
 
     const string date = utils::getTimeAsString();
+    cout << "Scan started at " << date << endl;
+    cout << "Vertical step " << VERTICAL_STEP
+         << ", measure interval " << MEASURE_INTERVAL_MS << " ms" << endl;
+
+    int totalCircles = 90 / VERTICAL_STEP;
 
     // TODO: Scan more than 90 degrees vertical
-    for (int i = 0; i < 90 / VERTICAL_STEP; i++) {
+    for (int i = 0; i < totalCircles; i++) {
+        const time_t start = time(nullptr);
+
+        cout << "------------" << endl;
+        cout << "Start Circle " << i + 1 << "/" << totalCircles << endl;
+
         if (i % 2 == 0) {
             // Drive to 360 degrees, taking measurements along the way
+            cout << "[Heading] Driving to 360 degrees" << endl;
             merlin.doSequenceStep(360, merlin.MOTOR_HEADING);
             measureLoop(merlin, lidar, measurements, MEASURE_INTERVAL_MS);
         } else {
             // Drive back to 0 degrees, taking measurements along the way
+            cout << "[Heading] Driving to 0 degrees" << endl;
             merlin.doSequenceStep(0, merlin.MOTOR_HEADING);
             measureLoop(merlin, lidar, measurements, MEASURE_INTERVAL_MS);
         }
 
         // Drive the upper motor up a bit
-        merlin.doSequenceStep(VERTICAL_STEP * (i + 1), merlin.MOTOR_PITCH);
+        const int pitchAngle = VERTICAL_STEP * (i + 1);
+        cout << "[Pitch] Driving to " << pitchAngle << " degrees" << endl;
+        merlin.doSequenceStep(pitchAngle, merlin.MOTOR_PITCH);
 
-
+        cout << "Circle done, writing 3D data" << endl;
         // For debugging: write a file after each circle
         for(const Measurement &m : measurements){
             measurements_3D.emplace_back(Calculation::get3DCoordinates(m));
@@ -82,6 +101,11 @@ int main(int argc, char **argv) {
         utils::writeBinaryPLY("3dPoints_" + date + "_" + std::to_string(i), measurements_3D);
         measurements.clear();
         measurements_3D.clear();
+        cout << "3D data written" << endl;
+
+        time_t elapsed = time(nullptr) - start;
+        cout << "Circle done in " << elapsed << " seconds" << endl;
+        cout << "Remaining time estimate: " << (totalCircles - (i + 1)) * elapsed << endl;
     }
 
 //    // Convert measurements into 3D coordinates
